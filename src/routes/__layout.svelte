a8a3b12c6b1a38511b23ab193ba4c289b0023426
z0ff <ex1999@msn.com>
2022-08-07 21:47:21 +0900

    changedd drawer design

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 	import '../app.css';
-	import { pageTitle } from '../store';
+	import { pageTitle, isDarkMode } from '../store';
     import { onMount } from 'svelte';
-import { element } from 'svelte/internal';
 
     let theme: string;
 	let y: number;
@@ -10,16 +9,16 @@ import { element } from 'svelte/internal';
 	let drawerContent: HTMLElement;
 
     onMount(() => {
-        let isDarkMode: boolean;
+        let isDark: boolean;
         const storageTheme = sessionStorage.getItem('theme');
         if (storageTheme == null) {
-            isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+            isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
         } else {
-            isDarkMode = storageTheme=="dark" ? true : false;
+            isDark = storageTheme=="dark" ? true : false;
         }
         const elem = document.getElementById("darkmode") as HTMLInputElement | null;
-        switchTheme(isDarkMode);
-        elem!.checked = isDarkMode;
+        switchTheme(isDark);
+        elem!.checked = isDark;
     })
 
 	pageTitle.subscribe((value) => {
@@ -32,6 +31,7 @@ import { element } from 'svelte/internal';
 
     function switchTheme(f: boolean) {
         theme = f ? "mydark" : "mylight";
+        isDarkMode.set(`${f}`);
     }
 
     function toggleDarkMode() {
@@ -64,7 +64,7 @@ import { element } from 'svelte/internal';
 		<!-- Page content here -->
 		<div
 			class={`navbar sticky top-0 z-40 bg-opacity-90 backdrop-blur ${
-				switchNavbarStyle || pageTitleValue != 'Home' ? 'bg-base-300' : ''
+				switchNavbarStyle ? 'bg-base-300' : ''
 			}`}
 		>
 			<div class="flex-none lg:hidden">
@@ -85,7 +85,7 @@ import { element } from 'svelte/internal';
 			</div>
 			{#if pageTitleValue != 'Home'}
 				<div class="flex-1 hidden lg:inline-block">
-					<span class="px-5 normal-case text-xl font-bold">
+					<span class="px-5 normal-case text-2xl font-bold">
 						{pageTitleValue}
 					</span>
 				</div>
@@ -101,24 +101,30 @@ import { element } from 'svelte/internal';
 				</a>
 			</div>
 			<div class="flex-none">
-				<label on:click={toggleDarkMode} class="swap swap-rotate">
+				<label on:click={toggleDarkMode} class="btn btn-square btn-ghost swap swap-rotate">
 					<!-- this hidden checkbox controls the state -->
 					<input id="darkmode" type="checkbox" />
 					<!-- sun icon -->
 					<svg
-						class="swap-off fill-current w-10 h-10"
+						class="swap-off fill-current"
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 24 24"
 						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
 							d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"
 						/></svg
 					>
 					<!-- moon icon -->
 					<svg
-						class="swap-on fill-current w-10 h-10"
+						class="swap-on fill-current"
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 24 24"
 						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
 							d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"
 						/></svg
 					>
@@ -186,19 +192,33 @@ import { element } from 'svelte/internal';
 	</div>
 	<div class="drawer-side">
 		<label for="my-drawer-2" class="drawer-overlay backdrop-blur" />
-		<div class="menu p-4 overflow-y-auto w-60 bg-neutral text-neutral-content bg-opacity-90">
-			<a href="/" class="btn btn-ghost h-max hidden lg:inline-block">
-				<div class="text-xl normal-case text-left">
+		<div class="p-4 overflow-y-auto w-60 bg-neutral text-neutral-content">
+			<a href="/" class="btn btn-ghost py-4 my-5 w-full h-max hidden lg:inline-block">
+				<div class="text-6xl normal-case text-center">
 					<div>Hello!</div>
-					<div class="text-xs">Sveltekit & daisyUI</div>
+					<div class="text-base">Sveltekit & daisyUI</div>
 				</div>
 			</a>
-			<!--<ul class="menu p-4 overflow-y-auto w-60 bg-neutral text-neutral-content">-->
-			<ul>
+			<ul class="menu overflow-y-auto">
 				<!-- Sidebar content here -->
-				<li><a on:click={toggleDrawer} href="/">Home</a></li>
-				<li><a on:click={toggleDrawer} href="/about">About</a></li>
-				<li><a on:click={toggleDrawer} href="/machines">Machines</a></li>
+				<li><a on:click={toggleDrawer} href="/">
+                    <div class="text-xl text-left">
+                        <div>Home</div>
+                        <div class="text-sm">ホーム</div>
+                    </div>
+                </a></li>
+				<li><a on:click={toggleDrawer} href="/about">
+                    <div class="text-xl text-left">
+                        <div>About</div>
+                        <div class="text-sm">このサイトについて</div>
+                    </div>
+                </a></li>
+				<li><a on:click={toggleDrawer} href="/machines">
+                    <div class="text-xl text-left">
+                        <div>Machines</div>
+                        <div class="text-sm">PC一覧</div>
+                    </div>
+                </a></li>
 			</ul>
 		</div>
 	</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,49 @@
 <script lang="ts">
-    import { pageTitle } from "../store";
-    pageTitle.set("Home");
+	import { pageTitle } from '../store';
+	pageTitle.set('Home');
 </script>
 
-<div class="w-full from-primary to-secondary text-primary-content -mt-[4rem] grid place-items-center items-end bg-gradient-to-br pt-20">
-	<div class="hero min-h-fit">
+<div
+	class="w-full from-primary to-secondary text-primary-content -my-[4rem] grid place-items-center items-end bg-gradient-to-br pt-20"
+>
+	<div class="hero min-h-screen pb-[8rem]">
 		<!--<div class="hero-overlay bg-opacity-60" />-->
 		<div class="hero-content text-neutral-content">
-			<div class="max-w-md">
-				<h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-				<p class="mb-5">
-					Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-					quasi. In deleniti eaque aut repudiandae et a id nisi.
-				</p>
-				<a href="/about" class="btn btn-outline">Get Started</a>
+			<div class="max-w-lg">
+				<h1 class="mb-5 text-9xl font-bold">Hello there.</h1>
+				<p class="mb-5 text-xl">めちゃ拙いWebサイト</p>
 			</div>
 		</div>
 	</div>
 </div>
 
-<div class="container p-5 w-full flex-grow">
+<div class="hero min-h-fit bg-base-200">
+	<div class="hero-content lg:p-20 w-full flex-col lg:flex-row-reverse">
+		<img src="https://placeimg.com/260/400/arch" class="m-5 max-w-sm rounded-lg shadow-2xl" />
+		<div class="flex-auto"></div>
+		<div class="text-center lg:text-left">
+			<h1 class="text-5xl font-bold">Webデザイン<br />なんもわからん</h1>
+			<p class="py-6">
+				SveltekitとTailwind CSS、daisyUIを用いて簡単なサイトを構築し、<br> Clowdflare
+				Pagesを用いてホスティングしてみました。
+			</p>
+			<a href="/about" class="btn btn-primary">MORE</a>
+		</div>
+	</div>
 </div>
+
+<div class="hero min-h-fit bg-base-200">
+	<div class="hero-content lg:p-20 w-full flex-col lg:flex-row">
+		<img src="https://placeimg.com/260/400/arch" class="m-5 flex-0 max-w-sm rounded-lg shadow-2xl" />
+		<div class="flex-auto"></div>
+		<div class="text-center lg:text-right">
+			<h1 class="text-2xl font-bold pb-6">お問い合わせはこちら</h1>
+			<button class="btn btn-primary">CONTACT</button>
+		</div>
+	</div>
+</div>
+
+<!--
+	<div class="container p-5 w-full flex-grow">
+	</div>
+-->
